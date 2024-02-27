7dad904ede62bb763ddf41a34a098dbdc529cca9
konstantin <kostya.ignatev.14@mail.ru>
2024-02-27 15:37:16 +0300

    В файле PersonList.vue в качестве ключа массива сделан person.id

--- src/components/PersonList.vue
+++ src/components/PersonList.vue
@@ -1,6 +1,6 @@
 <template lang="">
   <v-list density="compact">
-    <person-item class="pa-0" v-for="(person, i) in persons" :key="i"
+    <person-item class="pa-0" v-for="(person, i) in persons" :key="person.id"
     :person="person" color="primary" @delete="$emit('delete', person)" />
   </v-list>
 </template>
