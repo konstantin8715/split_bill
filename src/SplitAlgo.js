09c21fa720c62d6d713a7f82dbafe37fbf3ed9dd
konstantin <kostya.ignatev.14@mail.ru>
2024-02-28 12:36:54 +0300

    Функция SplitAlgo разбита на 3 функции и добавлен JSDoc

--- src/SplitAlgo.js
+++ src/SplitAlgo.js
@@ -1,7 +1,29 @@
 export const splitAlgo = function(positions) {
+    return calculateFinalDebts(calculateSumDebts(calculateAllDebts(positions)));
+}
+
+
+/**
+ * Функция по массиву позиций создает массив долгов для каждой позиции<br>
+ * Пример массива:
+ * <ul>
+ *  <li>Маша -> Дима 80 (1 позиция)</li>
+ *  <li>Саша -> Дима 80 (1 позиция)</li>
+ *  <li>Саша -> Маша 50 (2 позиция)</li>
+ *  <li>Дима -> Маша 50 (2 позиция)</li>
+ *  <li>Маша -> Дима 30 (3 позиция)</li>
+ *  <li>Саша -> Дима 30 (3 позиция)</li>
+ *  <li>Маша -> Саша 40 (4 позиция)</li>
+ *  <li>Дима -> Саша 40 (4 позиция)</li>
+ *  <li>Маша -> Саша 100 (5 позиция)</li>
+ *  <li>Дима -> Саша 100 (5 позиция)</li>
+ * </ul>
+ * @param {Array} positions - массив позиций меню
+ * @returns {Array} массив долгов для каждой позиции
+ */
+function calculateAllDebts(positions) {
+
     const allDebts = [];
-    const sumDebts = [];
-    const calculateDebts = [];
 
     for (const position of positions) {
         const splitPrice = position.price / position.persons.length;
@@ -19,6 +41,27 @@ export const splitAlgo = function(positions) {
         }
     }
 
+    return allDebts;
+}
+
+/**
+ * Функция по массиву долгов для каждой позиции создает массив сумм долгов<br>
+ * Пример массива (на основе примера из {@link calculateAllDebts}):
+ * <ul>
+ *  <li>Маша -> Дима 80+30=110</li>
+ *  <li>Маша -> Саша 40+100=140</li>
+ *  <li>Саша -> Дима 80+30=110</li>
+ *  <li>Саша -> Маша 50</li>
+ *  <li>Дима -> Саша 40+100=140</li>
+ *  <li>Дима -> Маша 50</li>
+ * </ul>
+ * @param {Array} allDebts - массив долгов для каждой позиции, полученный из {@link calculateAllDebts}
+ * @returns {Array} массив сумм долгов
+ */
+function calculateSumDebts(allDebts) {
+
+    const sumDebts = [];
+
     for (const debt of allDebts) {
         const currentDebt = sumDebts.find(
             (d) => d.from.id === debt.from.id && d.to.id === debt.to.id,
@@ -34,6 +77,24 @@ export const splitAlgo = function(positions) {
             currentDebt.sum = currentDebt.sum + debt.sum;
         }
     }
+
+    return sumDebts;
+}
+
+/**
+ * Функция по массиву сумм долгов создает массив итоговых долгов<br>
+ * Пример массива (на основе примера из {@link calculateSumDebts}):
+ * <ul>
+ *  <li>Маша -> Дима 60</li>
+ *  <li>Маша -> Саша 90</li>
+ *  <li>Дима -> Саша 30</li>
+ * </ul>
+ * @param {Array} sumDebts - массив сумм долгов, полученный из {@link calculateSumDebts}
+ * @returns {Array} массив итоговых долгов
+ */
+function calculateFinalDebts(sumDebts) {
+
+    const calculateDebts = [];
 
     for (const debt of sumDebts) {
         if (
